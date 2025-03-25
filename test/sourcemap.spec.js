e0dd0f921a191e9527468859b9ad99ad76e5281f
dvenckusvh <dvenckus@veritashealth.com>
2025-03-25 14:06:24 -0500

    Upgrade dependencies

--- test/sourcemap.spec.js
+++ test/sourcemap.spec.js
@@ -95,7 +95,7 @@ test('should ignore empty sourcemaps', t => {
             'there are two files'
           );
 
-          // eslint-disable-next-line no-console
+          // -- eslint-disable-next-line no-console
           console.log(lintResult.map(r => ({
             source: r.source,
             warnings: r.warnings[0]
